606c3c4f48346c6f78c0b136d426e93c55a4671e
jrheisler <jrheisler@yahoo.com>
2025-02-02 13:28:34 -0800

    Web version 1.6

--- content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.text) {
-        const utterance = new SpeechSynthesisUtterance(request.text);
-        speechSynthesis.speak(utterance);
-    }
-});
